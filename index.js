15a7ce99bc365bbfb8ec721fa4aea48e29d13f6b
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-02-01 16:44:28 -0600

    commit tree a1ed18815e42bded3bb0b7f6da2de42186f2ad86

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(namesArray, eventName){
+    let newArray = [ ]
+    for (let i = 0; i < namesArray.length; i++){
+        newArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArray
+}
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
